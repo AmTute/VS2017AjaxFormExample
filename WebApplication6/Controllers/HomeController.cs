424c91d943807c5dbb3a295d7b3be112c3bde390
Am Neu <amneutech@gmail.com>
2018-01-20 20:33:00 +0530

    Ajax Form In Visual Studio 2017

---
+++ WebApplication6/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication6.Models;
+using System.Threading;
+
+namespace WebApplication6.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        public IActionResult SaveForm(string Age, string Name)
+        {
+            Thread.Sleep(2000);
+            ViewBag.Age =Age;
+            ViewBag.Name = Name;
+            return View("About");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
